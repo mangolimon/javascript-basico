79d4f5725413836369f2eee862be72d1c5e654cf
mangolimon <116673476+mangolimon@users.noreply.github.com>
2022-11-13 20:33:38 +0100

    Add files via upload

---
+++ 02. Sintaxis variables y palabras reservadas/ejercicio02.js
@@ -0,0 +1,17 @@
+const lista = [
+    "M Ángeles",
+    34,
+    false,
+    new Date(1988,7,23),
+    {
+        titulo: "Teoría Optimista del Fracaso",
+        autor: "Ignasi Giró",
+        fecha: new Date(2019,13,3),
+        url: "https://www.amazon.es/Teor%C3%ADa-optimista-del-fracaso-tropezar-ebook/dp/B082DNDWJR",
+    },
+];
+
+console.log(lista)
+
+
+
